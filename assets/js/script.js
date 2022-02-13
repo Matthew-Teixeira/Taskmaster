541d40c12a0c2d1897b6a392e1410e0637881742
ApolloSolo <matt1teixeira@gmail.com>
2022-02-13 10:26:31 -0500

    Able to edit task and date data

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,30 +47,36 @@ var saveTasks = function () {
 
 //When click, p tag turns into textarea for editing
 $(".list-group").on("click", "p", function () {
+  //grab text data of p tag when it is clicked
   let text = $(this)
     .text()
     .trim();
+    
+  // create a textarea element and add class and value of text
   let textInput = $("<textarea>").addClass("form-control").val(text);
+
+  //replace this(<p>) with the new text area with the <p> text value
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
 });
 
 //when user clicks on anything but textarea, edited data is saved
 $(".list-group").on("blur", "textarea", function () {
+  //save text within text area (we are able o edit at this point)
   var text = $(this)
     .val()
     .trim();
 
-  // get the parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  // get the parent ul's id attribute aka: list-toDo then cut out list- so now it's just toDo. Fits our arrays
+  var status = $(this) //(this refers to the text area)
+    .closest(".list-group") //closest .list-group class is the ul
+    .attr("id") //Get ul id
+    .replace("list-", ""); // cut id down to toDo from list-toDo
 
   // get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+  var index = $(this) //(this refers to the text area)
+    .closest(".list-group-item")  //gives us the li "this" is in
+    .index(); //numbered index value
 
   //save the new data to tasks obj ex: toDo: [{text: "Run to moon base Alpha", date: "08/04/2055"}]
   tasks[status][index].text = text;
@@ -82,6 +88,37 @@ $(".list-group").on("blur", "textarea", function () {
   // replace textarea with p element
   $(this).replaceWith(taskP);
 });
+
+//When clicking on due date to edit
+$(".list-group").on("click", "span", function(){
+  let date = $(this).text().trim();
+
+  //create new input element
+  let dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
+
+  $(this).replaceWith(dateInput);
+
+  dateInput.trigger("focus");
+});
+
+//when user clicks outside of data, edited date is saved
+$(".list-group").on("blur", "input", function(){
+  let date = $(this).val().trim();
+
+  //get ul's id
+  let status = $(this).closest(".list-group").attr("id").replace("list-", "");
+
+  //get the task position in the list of other li elements
+  let index = $(this).closest(".list-group-item").index();
+
+  tasks[status][index].date = date;
+  saveTasks();
+
+  //replace text input with the previously used span
+  let taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
+
+  $(this).replaceWith(taskSpan);
+})
 
 
 // modal was triggered
